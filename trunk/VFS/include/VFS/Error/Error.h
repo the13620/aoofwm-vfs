37dbed8f75b58044d7b7fc20a8cab9f11d4adfca
laurent.malvert <laurent.malvert@de4390e2-0c24-0410-acf2-7d9395eead91>
2006-12-31 19:46:05 +0000

    source:  -updated VFS.Error    -> added some ErrorCode    -> Error Type

---
+++ trunk/VFS/include/VFS/Error/Error.h
@@ -0,0 +1,62 @@
+/*
+** This file is part of The Toy'd Project (http://www.toyd.org).
+**
+** The Toy'd Project is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** The Toy'd Project is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with The Toy'd Project; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+** This software has been written in EPITECH <http://www.epitech.net>
+** EPITECH is computer science school in Paris - FRANCE -
+** under the direction of Flavien Astraud <http://www.epita.fr/~flav>,
+** Jerome Landrieu and Launiau Severin.
+**
+** Project : The Toy'd Project
+**
+*/
+
+
+#ifndef __VFS_ERROR_ERROR_H__
+# define __VFS_ERROR_ERROR_H__
+
+# include "VFS/Error/API/IError.h"
+
+namespace	VFS
+{
+	namespace	Error
+	{
+		class	CError : public virtual VFS::Error::API::IError
+		{
+		private:
+			static CError*	_lastError;
+			
+		public:
+			std::string		_message;
+			eVFSErrorCode	_code;
+			
+		public:
+			CError(void);
+			CError(const eVFSErrorCode code = eveVFSError, const std::string& message = "");
+			virtual ~CError(void);
+			
+			const eVFSErrorCode	GetCode(void) const;
+			const std::string&	GetMessage(void) const;
+			
+			static CError*		GetError(void);
+		};
+	}
+}
+
+
+#endif	// __VFS_ERROR_ERROR_H__
